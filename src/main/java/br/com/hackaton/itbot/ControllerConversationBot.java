2084e769bfbf34ef45a9ae641d5cb491d8cc1f06
Anderson Silveira <anderson.silveira.br@gmail.com>
2017-03-24 09:38:25 -0300

    resource

--- src/main/java/br/com/hackaton/itbot/ControllerConversationBot.java
+++ src/main/java/br/com/hackaton/itbot/ControllerConversationBot.java
@@ -1,10 +1,18 @@
 package br.com.hackaton.itbot;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.api.domain.User;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
 import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageRequest;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
@@ -25,6 +33,24 @@ public class ControllerConversationBot {
 		System.out.println("wbehookResponse "+webhookResponse);
 		return webhookResponse;
 	}
+	    
+	    
+	 void createJira() throws URISyntaxException, IOException{
+	     final AsynchronousJiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
+	     URI uri = new URI("https://jira.cpqd.com.br");//https://jira.cpqd.com.br
+	    final JiraRestClient restClient = factory.createWithBasicHttpAuthentication(uri, "amorim", "cpqd@2015");
+	     try {
+	         final Issue issue = restClient.getIssueClient().getIssue("ETICS-153071").claim();
+	         User user = restClient.getUserClient().getUser("amorim").claim();
+	        // System.out.println(issue);
+	        	 System.out.println(user);
+	     }
+	     finally {
+	         // cleanup the restClient
+	         restClient.close();
+	     }
+	 }
 
+	
 	  
 }
