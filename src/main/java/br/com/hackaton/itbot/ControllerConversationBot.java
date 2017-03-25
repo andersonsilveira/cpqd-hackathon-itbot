c8600e3023bb22588db27aba32526b040840a1e3
Anderson Silveira <anderson.silveira.br@gmail.com>
2017-03-25 11:56:34 -0300

    alterações

--- src/main/java/br/com/hackaton/itbot/ControllerConversationBot.java
+++ src/main/java/br/com/hackaton/itbot/ControllerConversationBot.java
@@ -1,23 +1,17 @@
 package br.com.hackaton.itbot;
 
-import static org.elasticsearch.node.NodeBuilder.*;
-
 import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import org.elasticsearch.client.Client;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.node.Node;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import br.com.hackaton.jira.ConfluenceResponse;
 import br.com.hackaton.jira.ConfluenceService;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -31,32 +25,45 @@ import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
 @RequestMapping("/conversation")
 public class ControllerConversationBot {
 
-	@RequestMapping(method = RequestMethod.POST)
-	public @ResponseBody WebhookResponse conversation(@RequestBody String intentContent){
+    	private static Map<String, Map<String, Object>> sessionMap = new HashMap<String, Map<String,Object>>();
+    
+	@RequestMapping(method = RequestMethod.GET)
+	public @ResponseBody WebhookResponse conversation(@RequestParam(name="text") String intentContent, @RequestParam(name="conversationId",required=false) String conversationId){
 		ConversationService service = new ConversationService(ConversationService.VERSION_DATE_2016_07_11);
 		service.setUsernameAndPassword("6cd18a8a-bfd3-44ad-a14a-e23ce09605ab", "htALYxSVSRVw");
-		ObjectMapper objectMapper = new ObjectMapper();
-		JsonNode rootNode;
+		/*ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode rootNode;*/
 		try {
 			System.out.println(intentContent);
 			/*String split = intentContent.substring(5,intentContent.length()-2);
 			String jsonStr = split.replaceAll("\\\\", "");
-			System.out.println(jsonStr);*/
-			rootNode = objectMapper.readTree(intentContent.getBytes());
+			System.out.println(jsonStr);
+			rootNode = objectMapper.readTree(jsonStr.getBytes());
 			JsonNode contextNode = rootNode.path("context");
 			String text = rootNode.path("input").path("text").textValue();
+			MessageRequest newMessage = null;*/
+			//boolean isInitialContext = contextNode!=null && !contextNode.isNull();
+			System.out.println("conversationId "+conversationId);
+			boolean isInitialContext = conversationId==null || conversationId.isEmpty() || conversationId.equals("\"0\"");
 			MessageRequest newMessage = null;
-			boolean isInitialContext = contextNode!=null && !contextNode.isNull();
+			MessageResponse response = null;
 			if(isInitialContext){
-				newMessage = createMessageWithContext(contextNode, text);
+			        newMessage = new MessageRequest.Builder().inputText(intentContent).build();
+			        response = service.message("9b99c9e6-d597-4af7-a877-6f54e8315dec", newMessage).execute();
+			        Map<String, Object> context = response.getContext();
+			        System.out.println("contextoMpa "+context);
+			        sessionMap.put(context.get("conversation_id").toString(), context);
+			        System.out.println("Mapa 1 "+sessionMap.toString());
 				System.out.println(newMessage);
 			}else{
-				newMessage = createMessageWithNewContext(text); 
+			       System.out.println("Mapa 2 "+sessionMap.toString());
+			        newMessage = new MessageRequest.Builder().inputText(intentContent).context(sessionMap.get(conversationId)).build();
+			        response = service.message("9b99c9e6-d597-4af7-a877-6f54e8315dec", newMessage).execute();
+			       	
 			}
 
 
 
-		    MessageResponse response = service.message("9b99c9e6-d597-4af7-a877-6f54e8315dec", newMessage).execute();
 		    System.out.println(response);
 		    
 		    WebhookResponse webhookResponse = null;
@@ -74,11 +81,7 @@ public class ControllerConversationBot {
 			.setSource(putJsonDocument(webhookResponse)).execute().actionGet();
 			node.close();*/
 			return webhookResponse;
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
